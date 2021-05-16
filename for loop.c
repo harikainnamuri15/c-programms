a54bc87b44fe3c161dd08c8665c19d7a82696319
Harika <83266748+harikainnamuri15@users.noreply.github.com>
2021-05-16 11:08:20 +0530

    Add files via upload

---
+++ for loop.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf("%d ",i);
+	}
+	printf("\n");
+	for(i=n;i>=1;i--)
+	{
+		printf("%d ",i);
+	}
+	return 0;
+}
+//control statements:loops
+/*2 types:
+1.definite looping----->for
+2.indefinite looping---->while,do while
+reverse of a loop--->while loop*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
