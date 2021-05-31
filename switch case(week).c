3e38336b0b4a4a1205ad3c16de1b428b65a81efc
Harika <83266748+harikainnamuri15@users.noreply.github.com>
2021-05-31 11:32:31 +0530

    Add files via upload

---
+++ switch case(week).c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the vakue of n:");
+	scanf("%d",&n);
+	switch(n)
+	{
+		case 1:printf("sunday");
+		break;
+		case 2:printf("monday");
+		break;
+		case 3:printf("tuesday");
+		break;
+		case 4:printf("wednesday");
+		break;
+		case 5:printf("thursday");
+		break;
+		case 6:printf("friday");
+		break;
+		case 7:printf("saturday");
+		break;
+		default:printf("enter the n value between 1-7");
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
